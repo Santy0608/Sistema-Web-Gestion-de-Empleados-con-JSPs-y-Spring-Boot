fc451b5db852419fccb90426e0916ffdf6255694
Santiago <barquerotorress@gmail.com>
2024-09-09 00:24:01 -0600

    Creando el Modelo Vista Controlador

--- src/main/java/com/gm/sistema/empleados/controlador/IndexControlador.java
+++ src/main/java/com/gm/sistema/empleados/controlador/IndexControlador.java
@@ -1,4 +1,28 @@
 package com.gm.sistema.empleados.controlador;
 
+import com.gm.sistema.empleados.domain.Empleado;
+import com.gm.sistema.empleados.servicio.EmpleadoServicio;
+import com.gm.sistema.empleados.servicio.IEmpleadoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
 public class IndexControlador {
+
+    @Autowired
+    EmpleadoServicio empleadoServicio;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String iniciar(ModelMap modelo){
+        List<Empleado> empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado -> System.out.println(empleado.toString())));
+        //Compartimos modelo con la vista
+        modelo.put("empleados", empleados);
+        return "index";//index.jsp
+    }
 }

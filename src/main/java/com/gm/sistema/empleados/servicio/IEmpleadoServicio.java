eb1eade2c71a4f954600c8f64bf505e06d94bf7d
Santiago <barquerotorress@gmail.com>
2024-09-09 00:19:18 -0600

    Creando Clase Interface de Empleado Servicio

--- src/main/java/com/gm/sistema/empleados/servicio/IEmpleadoServicio.java
+++ src/main/java/com/gm/sistema/empleados/servicio/IEmpleadoServicio.java
@@ -1,5 +1,15 @@
 package com.gm.sistema.empleados.servicio;
 
+import com.gm.sistema.empleados.domain.Empleado;
+
+import java.util.List;
+
 public interface IEmpleadoServicio {
-    
+    public List<Empleado> listarEmpleados();
+
+    public Empleado buscarEmpleadoPorId(Integer id);
+
+    public void guardarEmpleado(Empleado empleado);
+
+    public void eliminarEmpleado(Empleado empleado);
 }

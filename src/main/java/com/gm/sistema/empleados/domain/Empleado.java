5d1423a48aad1de766fa86a28ef816fa16e8d944
Santiago <barquerotorress@gmail.com>
2024-09-09 00:15:11 -0600

    Creando Clase de Entidad Empleado con Spring

---
+++ src/main/java/com/gm/sistema/empleados/domain/Empleado.java
@@ -0,0 +1,14 @@
+package com.gm.sistema.empleados.domain;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Empleado {
+
+}

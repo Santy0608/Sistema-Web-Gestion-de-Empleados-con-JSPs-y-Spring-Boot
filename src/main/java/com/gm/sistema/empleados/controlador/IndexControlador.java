35a030f7eb3a71dea6120733480843c41c82a9f0
Santiago <barquerotorress@gmail.com>
2024-09-09 20:31:57 -0600

    Inicializando agregar.jsp y agregar.html

--- src/main/java/com/gm/sistema/empleados/controlador/IndexControlador.java
+++ src/main/java/com/gm/sistema/empleados/controlador/IndexControlador.java
@@ -3,9 +3,13 @@ package com.gm.sistema.empleados.controlador;
 import com.gm.sistema.empleados.domain.Empleado;
 import com.gm.sistema.empleados.servicio.EmpleadoServicio;
 import com.gm.sistema.empleados.servicio.IEmpleadoServicio;
+import jakarta.persistence.Index;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,6 +29,10 @@ public class IndexControlador {
         modelo.put("empleados", empleados);
         return "index";//index.jsp
     }
+
+
+
+
 
 
 }
